390617e3582b29f0d2132a2c01dfc7998947a9bb
Jorge Barnaby <jorge.barnaby@gmail.com>
2024-02-04 19:01:20 -0500

    Improve create-user in docker-registry

--- docker-registry/create-user.sh
+++ docker-registry/create-user.sh
@@ -13,6 +13,13 @@ fi
 # The first argument is the username
 username="$1"
 
+# Search for the string at the beginning of any line
+if grep -q "^${username}:" "$file_path"; then
+  echo "The user already exists in '$file_path'"
+  echo "Please choose another username or delete it from the file"
+  exit 1
+fi
+
 # Prompt for the password
 echo -n "Enter password for ${username}: "
 # -s flag hides the password input
@@ -27,12 +34,20 @@ mkdir -p "${dir_path}"
 
 # Check if the file exists
 if [ -f "${file_path}" ]; then
-  echo "File '${file_path}' already exists."
+  echo "File '${file_path}' already exists, appending to it."
 else
   # If the file does not exist, create it
   touch "${file_path}"
+  if [ $? -eq 0 ]; then
+    echo "File '${file_path}' has been created."
+  else
+    echo "Failed to create file '${file_path}'."
+  fi
 fi
 
 docker run \
   --entrypoint htpasswd \
+  --rm \
   httpd:2 -Bbn ${username} ${password} >> ${file_path}
+
+echo "User '${username}' has been created."
